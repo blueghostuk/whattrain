c31f1e87fd8d7f72cd2e7236706905ad90388f70
Michael Pritchard <blueghostuk@gmail.com>
2015-03-04 23:55:32 +0000

    try to fix scope #2

--- Scripts/app/StationLoader.js
+++ Scripts/app/StationLoader.js
@@ -17,32 +17,37 @@ var StationLoader = (function () {
             for (var j = 0; j < platform.segments.length; j++) {
                 var segment = platform.segments[j];
                 if (segment.berth) {
-                    promises.push(webApi.getBerthContents(segment.berth).then(function (berthData) {
-                        if (berthData) {
-                            if (berthData.m_Item3) {
-                                segment.train.reset();
-                                segment.train.id(berthData.m_Item2);
-                                var ts = moment(berthData.m_Item3.OriginDepartTimestamp).format(TrainNotifier.DateTimeFormats.dateQueryFormat);
-                                return webApi.getTrainMovementByUid(berthData.m_Item3.TrainUid, ts).done(function (train) {
-                                    segment.train.operator(train.Movement.Schedule.AtocCode.Name);
-                                });
-                            }
-                            else if (berthData.m_Item2) {
-                                segment.train.reset();
-                                segment.train.id(berthData.m_Item2);
-                            }
-                            else {
-                                segment.train.reset();
-                            }
-                        }
-                        else {
-                            segment.train.reset();
-                        }
-                    }));
+                    promises.push(this.getBerthContents(segment));
                 }
             }
         }
         return $.when(promises).always(function () { return $(".fa.fa-refresh").addClass("hide"); });
+    };
+    StationLoader.getBerthContents = function (segment) {
+        var obj = {
+            segment: segment,
+            getData: function () {
+                return webApi.getBerthContents(segment.berth).then(function (berthData) {
+                    if (berthData) {
+                        if (berthData.m_Item3) {
+                            obj.segment.train.reset();
+                            segment.train.id(berthData.m_Item2);
+                        }
+                        else if (berthData.m_Item2) {
+                            obj.segment.train.reset();
+                            obj.segment.train.id(berthData.m_Item2);
+                        }
+                        else {
+                            obj.segment.train.reset();
+                        }
+                    }
+                    else {
+                        obj.segment.train.reset();
+                    }
+                });
+            }
+        };
+        return obj.getData();
     };
     return StationLoader;
 })();
--- Scripts/app/StationLoader.ts
+++ Scripts/app/StationLoader.ts
@@ -17,29 +17,40 @@ class StationLoader {
             for (var j = 0; j < platform.segments.length; j++) {
                 var segment = platform.segments[j];
                 if (segment.berth) {
-                    promises.push(webApi.getBerthContents(segment.berth).then((berthData) => {
-                        if (berthData) {
-                            if (berthData.m_Item3) {
-                                segment.train.reset();
-                                segment.train.id(berthData.m_Item2);
-                                var ts = moment(berthData.m_Item3.OriginDepartTimestamp).format(TrainNotifier.DateTimeFormats.dateQueryFormat);
-                                return webApi.getTrainMovementByUid(berthData.m_Item3.TrainUid, ts).done((train) => {
-                                    segment.train.operator(train.Movement.Schedule.AtocCode.Name);
-                                });
-                            } else if (berthData.m_Item2) {
-                                segment.train.reset();
-                                segment.train.id(berthData.m_Item2);
-                            } else {
-                                segment.train.reset();
-                            }
-                        } else {
-                            segment.train.reset();
-                        }
-                    }));
+                    promises.push(this.getBerthContents(segment));
                 }
             }
         }
 
         return $.when(promises).always(() => $(".fa.fa-refresh").addClass("hide"));
+    }
+
+    private static getBerthContents(segment: PlatformSegment) {
+        var obj = {
+            segment: segment,
+            getData: () => {
+                return webApi.getBerthContents(segment.berth).then((berthData) => {
+                    if (berthData) {
+                        if (berthData.m_Item3) {
+                            obj.segment.train.reset();
+                            segment.train.id(berthData.m_Item2);
+                            /*var ts = moment(berthData.m_Item3.OriginDepartTimestamp).format(TrainNotifier.DateTimeFormats.dateQueryFormat);
+                            return webApi.getTrainMovementByUid(berthData.m_Item3.TrainUid, ts).done((train) => {
+                                segment.train.operator(train.Movement.Schedule.AtocCode.Name);
+                            });*/
+                        } else if (berthData.m_Item2) {
+                            obj.segment.train.reset();
+                            obj.segment.train.id(berthData.m_Item2);
+                        } else {
+                            obj.segment.train.reset();
+                        }
+                    } else {
+                        obj.segment.train.reset();
+                    }
+                });
+            }
+        };
+
+        return obj.getData();
     }
 } 
